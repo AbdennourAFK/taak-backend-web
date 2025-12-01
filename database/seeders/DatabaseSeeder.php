69d6b0c98ec275fdf05d1ffdcae8b4af21b8c10b
AbdennourAFK <abdennour.afkir@student.ehb.be>
2025-12-01 23:43:48 +0100

    Add categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        User::factory()->create([
+            "name"=> "Test User",
+            "email"=> "test@example.com",
+        ]
+            
+        );
         $categories=[
             "Technology",
             "Health",
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,28 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+    
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    
+
+<ul class="flex flex-wrap text-sm font-medium text-center text-body" justify-center>
+    <li class="me-2">
+        <a href="#" class="inline-block px-4 py-2.5 text-white bg-brand rounded-base active" aria-current="page">Tab 1</a>
+    </li>
+    <li class="me-2">
+        <a href="#"  class="inline-block px-4 py-3 rounded-base hover:text-heading hover:bg-neutral-secondary-soft">All</a>
+    </li>
+    @foreach ($categories as $category)
+    <li class="me-2">
+        <a href="#" class="inline-block px-4 py-3 rounded-base hover:text-heading hover:bg-neutral-secondary-soft">{{ $category->name }}</a>
+    </li>
+        
+    @endforeach
+    
+</ul>
+
                 </div>
             </div>
         </div>
